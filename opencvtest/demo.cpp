c0c3f6f2112000f3c3bf4ceabbf5c5bbbdde156d
Edith <yi.ping.edith@gmail.com>
2018-08-22 22:56:28 +1200

    update demo.cpp

--- opencvtest/demo.cpp
+++ opencvtest/demo.cpp
@@ -1,108 +1,135 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>      
-#include <opencv2/highgui/highgui.hpp>   
-#include "opencv2/highgui/highgui.hpp"
-#include <imgproc/imgproc.hpp> 
-#include <opencv2/opencv.hpp>  
-#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/core.hpp"      
+#include "opencv2/highgui.hpp"
+#include "opencv2/opencv.hpp"  
 #include "opencv2/imgproc/imgproc.hpp"  
+
 using namespace std;
 using namespace cv;
-//-----------------------------------【宏定义部分】--------------------------------------------  
-//  描述：定义一些辅助宏  
-//------------------------------------------------------------------------------------------------  
-#define WINDOW_NAME1 "Corner detection original"        //为窗口标题定义的宏  
-#define WINDOW_NAME2 "Corner detection gray"        //为窗口标题定义的宏  
+//-----------------------------------【Macro definition】--------------------------------------------  
+//  Description：Macro definition 
+//------------------------------------------------------------------------------------------------
+#define WINDOW_NAME_ORIG "Original Image"          
+#define WINDOW_NAME_GRAY "Gray Image"  
+#define WINDOW_NAME_CORD_COLOR "Corner detection original"   
+#define WINDOW_NAME_CORD_GRAY "Corner detection gray"       
+#define WINDOW_NAME_BINARY_IMG "Binary Image renderings" 
+#define WINDOW_NAME_EDGES "Edges detection gray" 
 
-//-----------------------------------【全局变量声明部分】--------------------------------------
-//		描述：全局变量声明
+//-----------------------------------【Global Variables】--------------------------------------
+//		Description：Global variable declaration
 //-----------------------------------------------------------------------------------------------
 Mat g_srcImage, g_srcImage1, g_grayImage;
-int thresh = 30; //当前阈值
-int max_thresh = 175; //最大阈值
+int thresh = 30; //threshold
+int max_thresh = 175; //max threshold
 
 
-//-----------------------------------【全局函数声明部分】--------------------------------------
-//		描述：全局函数声明
+//-----------------------------------【Global functions】--------------------------------------
+//		Description：Global function  declaration
 //-----------------------------------------------------------------------------------------------
-void on_CornerHarris(int, void*);//回调函数
+void on_CornerHarris(int, void*);//callback
 static void ShowHelpText();
+void showImageWindow(const String &winname, Mat imageArray);
+void showImageScalWindow(const String &winname, Mat imageArray, int rate);
 
-//-----------------------------------【main( )函数】--------------------------------------------
-//		描述：控制台应用程序的入口函数，我们的程序从这里开始执行
+//-----------------------------------【main( )】--------------------------------------------
+//		Description：Entry function
 //-----------------------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-	//【0】改变console字体颜色
+	//【0】change console font color
 	system("color 3F");
 
-	//【0】显示帮助文字
+	//【0】show help information
 	ShowHelpText();
 
-	//【1】载入原始图并进行克隆保存
-	g_srcImage = imread("G:\\open cv\\test\\opencvtest\\opencvtest\\1.jpg", 1);
-	if (!g_srcImage.data) { printf("Error reading image, please make sure that the image specified by imread function exists in the directory~！ \n"); return false; }
-	imshow("original", g_srcImage);
+	//【1】load and clone original image 
+	g_srcImage = imread("original/Hunder01.jpg", 1);
+	if (!g_srcImage.data) 
+	{
+		printf("Error reading image, please make sure that the image specified by imread function exists in the directory~！ \n");
+		return false; 
+	}
+	showImageWindow(WINDOW_NAME_ORIG, g_srcImage);
 	g_srcImage1 = g_srcImage.clone();
 
-	//【2】存留一张灰度图
+	//【2】Convert to gray and show it
 	cvtColor(g_srcImage1, g_grayImage, CV_BGR2GRAY);
+	showImageWindow(WINDOW_NAME_GRAY, g_grayImage);
 
-	//【3】创建窗口和滚动条
-	namedWindow(WINDOW_NAME1, CV_WINDOW_AUTOSIZE);
-	createTrackbar(" threshold value : ", WINDOW_NAME1, &thresh, max_thresh, on_CornerHarris);
-	//【4】调用一次回调函数，进行初始化
+	//【3】new window with scroll bar
+	namedWindow(WINDOW_NAME_CORD_COLOR, CV_WINDOW_FREERATIO);
+	resizeWindow(WINDOW_NAME_CORD_COLOR, g_srcImage.cols / 1, g_srcImage.rows / 1);  //notice: special process to be consistent with other windows
+	createTrackbar(" threshold value : ", WINDOW_NAME_CORD_COLOR, &thresh, max_thresh, on_CornerHarris);
+
+	//【4】Initiation, evoke callback function on_CornerHarris()
 	on_CornerHarris(0, 0);
-	//以灰度模式载入图像并显示
-	Mat srcImage = imread("G:\\open cv\\test\\opencvtest\\opencvtest\\1.jpg", 0);
-	imshow("gray", srcImage);
 
-	//进行Harris角点检测找出角点
+	//Harris Corner Detection
 	Mat cornerStrength;
-	cornerHarris(srcImage, cornerStrength, 2, 3, 0.01);
+	cornerHarris(g_grayImage, cornerStrength, 2, 3, 0.01);
 
-	//对灰度图进行阈值操作，得到二值图并显示  
+	//get binary image by shresholding on gray image
 	Mat harrisCorner;
 	threshold(cornerStrength, harrisCorner, 0.00001, 255, THRESH_BINARY);
-	imshow("Binary Image renderings after corner detection", harrisCorner);
-	Mat contours;
-	Canny(srcImage, contours, 125, 350);   //灰度图 //输出轮廓//低阈值  //高阈值  
+	showImageWindow(WINDOW_NAME_BINARY_IMG, harrisCorner);
 
-	//因为正常情况下轮廓是用非零像素表示 我们反转黑白值  
-	Mat contoursInv;    //反转后的图像  
-	threshold(contours, contoursInv, 128, 255, THRESH_BINARY_INV);//低于该值的像素 //将变成255  
-	imshow("contoursInv", contoursInv);
+	//edges detection using Canny algorithm
+	Mat contours;           // edges output
+	int low_valve = 100;    // low threshold value
+	int high_valve = 350;   // high threshold value
+	Canny(g_grayImage, contours, low_valve, high_valve);
+
+	//Invert to show edges in 255   
+	Mat contoursInv;    //output Inverted Contour Image  
+	threshold(contours, contoursInv, 128, 255, THRESH_BINARY_INV);//threshold:128, all values below it will becom 255
+	showImageWindow(WINDOW_NAME_EDGES, contoursInv);
+
 	waitKey(0);
 	return 0;
 	
 }
+void showImageScalWindow(const String &winname, Mat imageArray, int rate)
+{
+	if (rate == 0)
+		return;
+	namedWindow(winname, CV_WINDOW_FREERATIO);
+	resizeWindow(winname, imageArray.cols / rate, imageArray.rows / rate);
+	imshow(winname, imageArray);
+}
 
-//-----------------------------------【on_HoughLines( )函数】--------------------------------
-//		描述：回调函数
+void showImageWindow(const String &winname, Mat imageArray)
+{
+	int rate = 1;
+	showImageScalWindow(winname, imageArray, rate);
+}
+
+
+//-----------------------------------【callback function: on_CornerHarris( )】--------------------------------
+//		description：callback function for trackbar
 //----------------------------------------------------------------------------------------------
 
 void on_CornerHarris(int, void*)
 {
-	//---------------------------【1】定义一些局部变量-----------------------------
-	Mat dstImage;//目标图
-	Mat normImage;//归一化后的图
-	Mat scaledImage;//线性变换后的八位无符号整型的图
+	//---------------------------【1】local variable definition-----------------------------
+	Mat dstImage;//output image
+	Mat normImage;//normalized image
+	Mat scaledImage;//8-bit image after scaling, taking absolute value
 
-	//---------------------------【2】初始化---------------------------------------
-	//置零当前需要显示的两幅图，即清除上一次调用此函数时他们的值
+	//---------------------------【2】initializtion---------------------------------------
 	dstImage = Mat::zeros(g_srcImage.size(), CV_32FC1);
 	g_srcImage1 = g_srcImage.clone();
 
-	//---------------------------【3】正式检测-------------------------------------
-	//进行角点检测
+	//---------------------------【3】detection-------------------------------------
+	//Harris Corner Detection
 	cornerHarris(g_grayImage, dstImage, 2, 3, 0.04, BORDER_DEFAULT);
 
-	// 归一化与转换
+	// normalization and scaling
 	normalize(dstImage, normImage, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
-	convertScaleAbs(normImage, scaledImage);//将归一化后的图线性变换成8位无符号整型 
+	convertScaleAbs(normImage, scaledImage);
 
-	//---------------------------【4】进行绘制-------------------------------------
-	// 将检测到的，且符合阈值条件的角点绘制出来
+	//---------------------------【4】mark corners -------------------------------------
+	// draw detected corners
 	for (int j = 0; j < normImage.rows; j++)
 	{
 		for (int i = 0; i < normImage.cols; i++)
@@ -114,278 +141,20 @@ void on_CornerHarris(int, void*)
 			}
 		}
 	}
-	//---------------------------【4】显示最终效果---------------------------------
-	imshow(WINDOW_NAME1, g_srcImage1);
-	imshow(WINDOW_NAME2, scaledImage);
+	//---------------------------【4】show results ---------------------------------
+	showImageWindow(WINDOW_NAME_CORD_COLOR, g_srcImage1);
+	showImageWindow(WINDOW_NAME_CORD_GRAY, scaledImage);
 
 }
 
-//-----------------------------------【ShowHelpText( )函数】----------------------------------
-//		描述：输出一些帮助信息
+//-----------------------------------【ShowHelpText( )】----------------------------------
+//		description：output some help information
 //----------------------------------------------------------------------------------------------
 static void ShowHelpText()
 {
-	//输出一些帮助信息
+	//output some help information
 	printf("\n\n\n\t\t\t【Image detection~!】\n\n");
 	printf("\n\n\n\tPlease adjust the scroll bar to observe the image effect~\n\n");
 	printf("\n\n\t\t\t\t\t\t\t\t by group 2"
 	);
 }
-/*
-//-----------------------------------【头文件包含部分】----------------------------------------- 
-//    描述：包含程序所依赖的头文件 
-//---------------------------------------------------------------------------------------------- 
-#include <cv.h> 
-#include <highgui.h> 
-#include "opencv2/core/core.hpp" 
-#include "opencv2/imgproc/imgproc.hpp" 
-#include "opencv2/highgui/highgui.hpp" 
-#include <iostream>
-
-//-----------------------------------【命名空间声明部分】-------------------------------------- 
-//    描述：包含程序所使用的命名空间 
-//--------------------------------------------------------------------------------------------- 
-using namespace std;
-using namespace cv;
-
-//-----------------------------------【全局变量声明部分】-------------------------------------- 
-//    描述：全局变量声明 
-//--------------------------------------------------------------------------------------------- 
-int g_nContrastValue; //对比度值 
-int g_nBrightValue;  //亮度值 
-int Thresh = 50;
-
-//-----------------------------------【全局函数声明部分】-------------------------------------- 
-//    描述：全局函数声明 
-//--------------------------------------------------------------------------------------------- 
-static void ContrastAndBright(int);
-void   ShowHelpText();
-
-//-----------------------------------【声明图像IplImage指针】---------------------------------- 
-//描述：声明图像IplImage指针 
-//--------------------------------------------------------------------------------------------- 
-IplImage* g_pSrcImg = NULL;
-
-IplImage* g_pGrayImage = NULL;
-
-IplImage* g_pCannyImg = NULL;
-
-IplImage *g_pBinaryImage = NULL;
-
-IplImage *g_pDstImage = NULL;
-
-
-
-//滑动条响应函数
-
-void onTrackerSlid(int thresh)
-
-{
-
-	cvCanny(g_pGrayImage, g_pCannyImg, (float)thresh, (float)thresh * 3, 3);
-
-	cvShowImage("Canny Image", g_pCannyImg);
-
-}
-
-//-----------------------------【on_trackbar( )函数】------------------------------------ 
-//    描述：图像二值化阈值回调函数 
-//----------------------------------------------------------------------------------------------- 
-void on_trackbar(int pos)
-{
-	// 转为二值图  
-	cvThreshold(g_pGrayImage, g_pBinaryImage, pos, 255, CV_THRESH_BINARY);
-	// 显示二值图  
-	cvShowImage("Binary Image", g_pBinaryImage);
-}
-
-
-//-----------------------------------【main( )函数】-------------------------------------------- 
-//    描述：控制台应用程序的入口函数，程序从这里开始执行 
-//----------------------------------------------------------------------------------------------- 
-int main(int argc, char** argv)
-
-{
-	system("color 5F");
-	ShowHelpText();
-	if (argc != 2)
-
-		argv[1] = "G:\\open cv\\test\\opencvtest\\opencvtest\\1";
-
-	//设定对比度和亮度的初值
-
-	g_nContrastValue = 80;
-	g_nBrightValue = 80;
-
-	//载入图像，强制转化为Gray
-
-	if ((g_pSrcImg = cvLoadImage(argv[1], 1)) != 0)
-
-	{
-
-		cout << "Press ESC to Quit" << endl;
-
-		//将颜色空间由RGB转化为Gray
-
-		g_pGrayImage = cvCreateImage(cvGetSize(g_pSrcImg), 8, 1);
-
-		cvCvtColor(g_pSrcImg, g_pGrayImage, CV_RGB2GRAY);
-
-		// 创建二值图  
-		g_pBinaryImage = cvCreateImage(cvGetSize(g_pGrayImage), IPL_DEPTH_8U, 1);
-
-		//canny边缘检测
-
-		g_pCannyImg = cvCreateImage(cvGetSize(g_pGrayImage), IPL_DEPTH_8U, 1);
-
-		cvNamedWindow("ContrastandBrightnessWnd", CV_WINDOW_AUTOSIZE);
-
-		//创建轨迹条
-
-		cvCreateTrackbar("Contrast：", "ContrastandBrightnessWnd", &g_nContrastValue, 300, ContrastAndBright);
-		cvCreateTrackbar("Brightness：", "ContrastandBrightnessWnd", &g_nBrightValue, 200, ContrastAndBright);
-
-
-		//调用回调函数 
-		ContrastAndBright(g_nContrastValue);
-		ContrastAndBright(g_nBrightValue);
-
-		//创建窗口
-
-		cvNamedWindow("Source Image", CV_WINDOW_AUTOSIZE);
-
-		cvNamedWindow("Gray Image", CV_WINDOW_AUTOSIZE);
-
-		cvNamedWindow("Canny Image", CV_WINDOW_AUTOSIZE);
-
-		cvNamedWindow("Binary Image", CV_WINDOW_AUTOSIZE);
-
-
-		// 滑动条    
-		int nThreshold = 0;
-		cvCreateTrackbar("Threshold：", "Binary Image", &nThreshold, 254, on_trackbar);
-		on_trackbar(1);
-
-		//添加滑动条来调节边缘检测的阈值
-
-		onTrackerSlid(Thresh);
-
-		cvShowImage("Gray Image", g_pCannyImg);
-
-		cvCreateTrackbar("Threshold", "Canny Image", &Thresh, 300, onTrackerSlid);
-
-		//显示图像
-
-		cvShowImage("Source Image", g_pSrcImg);
-
-		cvShowImage("Gray Image", g_pGrayImage);
-
-		//保存图像
-
-		cvSaveImage("Gray_Image.jpg", g_pGrayImage);
-
-		cvSaveImage("Canny_Image.jpg", g_pCannyImg);
-
-		cvSaveImage("Binary Image.jpg", g_pBinaryImage);
-
-
-		//等待按"ESC"键退出
-
-		while (1)
-
-			if (cvWaitKey(100) == 27)
-
-				break;
-
-		//销毁窗口
-
-		cvWaitKey(0);
-
-		cvDestroyWindow("Source Image");
-
-		cvDestroyWindow("Canny Image");
-
-		cvDestroyWindow("Gray Image");
-
-		cvDestroyWindow("Binary Image");
-
-		//释放图像
-
-		cvReleaseImage(&g_pGrayImage);
-
-		cvReleaseImage(&g_pCannyImg);
-
-		cvReleaseImage(&g_pSrcImg);
-
-		cvReleaseImage(&g_pBinaryImage);
-
-		return 0;
-
-	}
-
-	return -1;
-
-}
-
-
-//-----------------------------【ContrastAndBright( )函数】------------------------------------ 
-//    描述：改变图像对比度和亮度值的回调函数 
-//--------------------------------------------------------------------------------------------- 
-static void ContrastAndBright(int)
-{
-
-	// 三个for循环，执行运算 dstImg(i,j) = a*srcImg(i,j) + b 
-	Mat srcImg, dstImg;
-	srcImg = imread("lena.jpg");
-	if (!srcImg.data)
-	{
-		printf("读取srcImg图片错误~！ \n");
-		return;
-	}
-	dstImg = Mat::zeros(srcImg.size(), srcImg.type());
-
-	for (int y = 0; y < srcImg.rows; y++)
-	{
-		for (int x = 0; x < srcImg.cols; x++)
-		{
-			for (int c = 0; c < 3; c++)
-			{
-				dstImg.at<Vec3b>(y, x)[c] = saturate_cast<uchar>((g_nContrastValue*0.01)*(srcImg.at<Vec3b>(y, x)[c]) + g_nBrightValue);
-			}
-		}
-	}
-
-	// 显示图像 
-	imshow("ContrastandBrightnessWnd", dstImg);
-}
-
-//-----------------------------------【ShowHelpText( )函数】---------------------------------- 
-//         描述：输出一些帮助信息 
-//---------------------------------------------------------------------------------------------- 
-void ShowHelpText()
-{
-
-	printf("\n\n\t\t\t                运行平台为win32 VS2013\n");
-	printf("\n\n\t\t\t                OpenCV一个值得你学习的强大视觉库\n");
-	printf("\n\n\t\t\t                当前使用的OpenCV版本为：" CV_VERSION);
-	printf("\n\n  ------------------------------------------------------------------------------------------------------------------\n");
-}#include<opencv2/opencv.hpp>
-#include<iostream>
-using namespace std;
-using namespace cv;
-
-int main()
-{
-	Mat image2 = imread("G:\\open cv\\test\\opencvtest\\opencvtest\\tp2.jpg");
-	imshow("显示图片2", image2);
-	waitKey(0);
-	return 0;
-}
-import cv2
-{
-	image = cv2.imread("G:\\open cv\\test\\opencvtest\\opencvtest\\tp2.jpg");
-	cv2.imshow("测试", image);
-	image = cv2.Canny(image, 200, 300);
-	cv2.imshow("测试2", image);
-	cv2.waitKey(0);
-}*/
